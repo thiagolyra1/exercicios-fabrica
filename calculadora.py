c81257498eaf0f6dfa4d2c5e7df4a4275028a353
Thiago de Lyra Borges <thiagolyra1@gmail.com>
2024-08-20 15:28:14 -0300

    nova lógica do while de dentro

--- calculadora.py
+++ calculadora.py
@@ -7,8 +7,7 @@ while True:
         print("Você não digitou um número.")
         continue
     
-    retornar_operacao = True
-    while retornar_operacao:
+    while True:
         # Operação a ser utilizada na calculadora
         operacao = input("Qual operação você gostaria de realizar? Escolha algum desses: '+', '-', 'x' ou '/': ")
 
@@ -22,19 +21,19 @@ while True:
         
         if operacao == "+":
             print(f'O resultado de {numero1} + {numero2} é:  {adicao}')
-            retornar_operacao = False
+            break
         elif operacao == "-":
             print(f'O resultado de {numero1} - {numero2} é:  {subtracao}')
-            retornar_operacao = False
+            break
         elif operacao == "x":
             print(f'O resultado de {numero1} x {numero2} é: {multiplicacao}')
-            retornar_operacao = False
+            break
         elif operacao =="/":
             print(f'O resultado de {numero1} / {numero2} é: {divisao}')
-            retornar_operacao = False
+            break
         else:
             print("Não é uma operação válida, escolha novamente.")
-            retornar_operacao = True
+            continue
     
     # Pergunta para continuar ou sair do projeto
     sair = input("Você deseja sair da calculadora? [s]im ou [n]ão: ")
